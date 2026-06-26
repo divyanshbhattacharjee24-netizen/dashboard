73fac4cd8ec5bbe2f8e3e67218edc6c626a5a587
divyanshbhattacharjee24-netizen <divyanshbhattacharjee24@gmail.com>
2026-06-26 16:36:08 +0530

    Update app.py
    changed app.py

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(name)
+app = Flask(__name__)
 
 BOT_INVITE = "https://discord.com/oauth2/authorize?client_id=1398895385711349822&permissions=8&scope=bot"
 
 @app.route("/")
 def home():
-try:
-data = requests.get(
-"https://discord-bot-production-cc86.up.railway.app",
-timeout=5
-).json()
+    try:
+        data = requests.get(
+            "https://discord-bot-production-cc86.up.railway.app/stats",
+            timeout=5
+        ).json()
 
-    server_count = data.get("servers", 0)
+        server_count = data.get("servers", 0)
 
-except Exception:
-    server_count = "Offline"
+    except Exception:
+        server_count = "Offline"
 
-return render_template(
-    "index.html",
-    bot_invite=BOT_INVITE,
-    server_count=server_count
-)
+    return render_template(
+        "index.html",
+        bot_invite=BOT_INVITE,
+        server_count=server_count
+    )
 
-if name == "main":
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
